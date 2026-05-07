0f8b1f0e1c2c4a4221d324db4faa1025444d0319
sustenablet <sustenablet@gmail.com>
2026-05-07 19:55:11 -0400

    Initial Commit

---
+++ Bob/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Bob
+//
+//  Created by Joao Leite on 5/7/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BobTests/BobTests.swift
@@ -0,0 +1,19 @@
+//
+//  BobTests.swift
+//  BobTests
+//
+//  Created by Joao Leite on 5/7/26.
+//
+
+import Testing
+@testable import Bob
+
+struct BobTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
